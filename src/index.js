dc20f3d23b942ddf9b7644cf23a91f46f0f6e96c
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-03-17 01:03:48 +0200

    added GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <BrowserRouter basename='/VocabBuilder'>
+      <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
+      <GlobalStyle/>
     </BrowserRouter>
   </React.StrictMode>
 );
