69ec8e53a6dc2e34b0c358a0ee9dc4533d82a785
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2024-03-21 18:15:00 +0000

    Deploying to gh-pages from @ aniakurtiak/VocabBuilder@2b8bcecb3dbfedd12a97c2b7427070bda911c7a5 🚀

---
+++ static/js/270.12c99418.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[270],{7270:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"RecommendPage"})}}}]);
+//# sourceMappingURL=270.12c99418.chunk.js.map
---
+++ static/js/606.887ed0cc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[606],{2606:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"DictionaryPage"})}}}]);
+//# sourceMappingURL=606.887ed0cc.chunk.js.map
