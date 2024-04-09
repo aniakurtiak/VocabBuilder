9f934d692902f0e4c7a7e1f92a1691deda0d761f
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2024-04-09 13:30:13 +0000

    Deploying to gh-pages from @ aniakurtiak/VocabBuilder@e8b62944566a0433a4c4bf454a6bebdaea6ed8de 🚀

--- static/js/405.36a92342.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_create_app=self.webpackChunkreact_create_app||[]).push([[405],{7405:function(e,a,r){r.r(a);r(2791);var c=r(184);a.default=function(){return(0,c.jsx)("div",{children:"TrainingPage"})}}}]);
-//# sourceMappingURL=405.36a92342.chunk.js.map
